5847540e36022af6c943efcc77b88367d59c9cf3
claire <github email address>
2019-01-07 23:18:40 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+// 
+function findMatching(names, name){
+    return names.filter(function(n){
+        return n.toLowerCase()===name.toLowerCase()
+    });
+};
+    
+function fuzzyMatch(names, letters){
+    return names.filter(function(n) {
+        return n.startsWith(letters)
+    });
+};
+
+function matchName(names, str){
+    return names.filter(function(n){
+        return n.name === str
+    });
+};
